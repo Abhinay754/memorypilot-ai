ed9bc7228dbed50e47d64c0d388bc341da101c51
Abhinay <abhinaykatariya66@gmail.com>
2026-07-01 19:28:15 +0530

    Fix PDF upload

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getDocument } from "pdfjs-dist";
+import pdfParse from "pdf-parse";
 import db from "@/lib/db";
 
 export async function POST(req: NextRequest) {
@@ -17,37 +17,21 @@ export async function POST(req: NextRequest) {
 
     const buffer = Buffer.from(await file.arrayBuffer());
 
-   // const pdf = await pdfParse(buffer);
-   const pdf = await getDocument({ data: buffer }).promise;
-
-let text = "";
-
-for (let i = 1; i <= pdf.numPages; i++) {
-  const page = await pdf.getPage(i);
-
-  const content = await page.getTextContent();
-
-  text +=
-    content.items
-      .map((item: any) => item.str)
-      .join(" ") + "\n";
-}
-
-console.log("Extracted Text:");
-console.log(text);
+    const pdf = await pdfParse(buffer);
+    const text = pdf.text;
 
     db.prepare(
       "INSERT INTO memories (role, message) VALUES (?, ?)"
     ).run("document", text);
 
     db.prepare(`
-  INSERT INTO documents (filename, content)
-  VALUES (?, ?)
-`).run(file.name, text);
+      INSERT INTO documents (filename, content)
+      VALUES (?, ?)
+    `).run(file.name, text);
 
     return NextResponse.json({
       success: true,
-      text: text,
+      text,
     });
   } catch (err) {
     console.error("PDF Upload Error:", err);
